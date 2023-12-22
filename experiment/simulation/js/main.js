c09cbb8575e421378586f1e40edcde5d0c0ee4e9
prakriti5dhang <prakritidhang@gmail.com>
2023-12-22 15:18:32 +0530

    itc

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -9,8 +9,10 @@ Developer: Prakriti Dhang
 function start() {
    
   document.getElementById("step0").disabled = true;
- // document.getElementById("step1").disabled = false;
-  document.getElementById("plungerinj").setAttribute("onclick", "injectplunger()");
+  document.getElementById("int1").disabled = false;
+  document.getElementById("step1").disabled = false;
+  document.getElementById("step2").disabled = false;
+  
 }
 
 function restartexp() {
@@ -29,20 +31,35 @@ function showligand(){
 
 }
 
+function showsamplebtn(){
+  document.getElementById("protein").style.display="block";
+
+}
+
+function showligandbtn(){
+  document.getElementById("ligand").style.display="block";
+  
+
+}
+
+
+
 
 
 var imgobjplunger=null;
 function injectplunger(){
- var plungertop = -3;
+ var plungertop = 1;
  var imgplunger= document.getElementById("plungerinj");
  clearInterval(imgobjplunger);
- imgobjplunger = setInterval(frame, 60);
+ imgobjplunger = setInterval(frame, 90);
 
 
  function frame(){
-  if(plungertop == 11){
+  if(plungertop == 4){
     clearInterval(imgobjplunger);
     document.getElementById("plungerinj").removeAttribute("onclick", "injectplunger()");
+    document.getElementById("int2").disabled = false;
+    document.getElementById("int10").style.display="block";
   }
   else{
     plungertop++;
@@ -51,7 +68,7 @@ function injectplunger(){
     canvass11 = document.getElementById("injectioncolumn");
     ctxgs11 = canvass11.getContext("2d");
     var posY = 0;
-    var speed = 0.4;
+    var speed = 0.5;
 
 
 
@@ -93,13 +110,18 @@ function injectplunger(){
 
 function clickchk1(){
   if(document.getElementById("int1").checked){
-    document.getElementById("int10").style.display="block";
+    document.getElementById("plungerinj").setAttribute("onclick", "injectplunger()");
+    document.getElementById("ligand").style.display="none";
+    document.getElementById("protein").style.display="none";
     document.getElementById("int20").style.display="none";
     document.getElementById("int30").style.display="none";
     document.getElementById("int40").style.display="none";
     document.getElementById("int2").checked=false;
     document.getElementById("int3").checked=false;
     document.getElementById("int4").checked=false;
+    document.getElementById("int2").disabled=true;
+    document.getElementById("int3").disabled=true;
+    document.getElementById("int4").disabled=true;
   }
   else{
     $('#alertModal').modal('show');
@@ -110,13 +132,17 @@ function clickchk1(){
 
 function clickchk2(){
   if(document.getElementById("int2").checked){
-    document.getElementById("int20").style.display="block";
+    document.getElementById("plungerinj").setAttribute("onclick", "injectplunger2()");
+   // document.getElementById("int20").style.display="block";
     document.getElementById("int10").style.display="none";
     document.getElementById("int30").style.display="none";
     document.getElementById("int40").style.display="none";
     document.getElementById("int1").checked=false;
     document.getElementById("int3").checked=false;
     document.getElementById("int4").checked=false;
+    document.getElementById("int4").disabled=true;
+    document.getElementById("int3").disabled=true;
+    document.getElementById("int1").disabled=true;
 
   }
   else{
@@ -128,13 +154,17 @@ function clickchk2(){
 
 function clickchk3(){
   if(document.getElementById("int3").checked){
-    document.getElementById("int30").style.display="block";
+    document.getElementById("plungerinj").setAttribute("onclick", "injectplunger3()");
+    //document.getElementById("int30").style.display="block";
     document.getElementById("int10").style.display="none";
     document.getElementById("int20").style.display="none";
     document.getElementById("int40").style.display="none";
     document.getElementById("int2").checked=false;
     document.getElementById("int1").checked=false;
     document.getElementById("int4").checked=false;
+    document.getElementById("int2").disabled=true;
+    document.getElementById("int4").disabled=true;
+    document.getElementById("int1").disabled=true;
   }
   else{
     $('#alertModal').modal('show');
@@ -144,13 +174,17 @@ function clickchk3(){
 }
 function clickchk4(){
   if(document.getElementById("int4").checked){
-    document.getElementById("int40").style.display="block";
+    document.getElementById("plungerinj").setAttribute("onclick", "injectplunger4()");
+    //document.getElementById("int40").style.display="block";
     document.getElementById("int10").style.display="none";
     document.getElementById("int30").style.display="none";
     document.getElementById("int20").style.display="none";
     document.getElementById("int2").checked=false;
     document.getElementById("int3").checked=false;
     document.getElementById("int1").checked=false;
+    document.getElementById("int2").disabled=true;
+    document.getElementById("int3").disabled=true;
+    document.getElementById("int1").disabled=true;
   }
   else{
     $('#alertModal').modal('show');
@@ -166,6 +200,181 @@ function cancelmsg() {
   document.getElementById("alertModal").classList.remove("show");
 }
 
+
+var imgobjplunger2=null;
+function injectplunger2(){
+ var plungertop = 4;
+ var imgplunger2= document.getElementById("plungerinj");
+ clearInterval(imgobjplunger2);
+ imgobjplunger2 = setInterval(frame, 90);
+
+
+ function frame(){
+  if(plungertop == 7){
+    clearInterval(imgobjplunger2);
+    document.getElementById("plungerinj").removeAttribute("onclick", "injectplunger2()");
+    document.getElementById("int20").style.display="block";
+    document.getElementById("int3").disabled = false;
+  }
+  else{
+    plungertop++;
+    imgplunger2.style.top=plungertop + "%";
+
+    canvass11 = document.getElementById("injectioncolumn2");
+    ctxgs11 = canvass11.getContext("2d");
+    var posY = 0;
+    var speed = 0.5;
+
+
+
+    function drawLine() {
+
+
+      ctxgs11.strokeStyle = 'white';
+      ctxgs11.lineWidth = 600;
+      ctxgs11.beginPath();
+      ctxgs11.moveTo(0, posY); /*  */
+      ctxgs11.lineTo(0, 0);
+      ctxgs11.stroke();
+    }
+
+    function moveLine() {
+      posY += speed;
+
+      if (posY < 0 || posY > canvass11.height) {
+        speed = speed * -1;
+      }
+    }
+
+    function loop() {
+      // clear old frame;
+      //ctxgs11.clearRect(0,0,canvass11.width, canvass11.height);
+      moveLine();
+      drawLine();
+      cancelani = requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
+  }
+ }
+
+}
+
+
+var imgobjplunger3=null;
+function injectplunger3(){
+ var plungertop = 7;
+ var imgplunger3= document.getElementById("plungerinj");
+ clearInterval(imgobjplunger3);
+ imgobjplunger3 = setInterval(frame, 80);
+
+
+ function frame(){
+  if(plungertop == 10){
+    clearInterval(imgobjplunger3);
+    document.getElementById("plungerinj").removeAttribute("onclick", "injectplunger3()");
+    document.getElementById("int30").style.display="block";
+    document.getElementById("int4").disabled = false;
+  }
+  else{
+    plungertop++;
+    imgplunger3.style.top=plungertop + "%";
+
+    canvass11 = document.getElementById("injectioncolumn3");
+    ctxgs11 = canvass11.getContext("2d");
+    var posY = 0;
+    var speed = 0.5;
+
+
+
+    function drawLine() {
+
+
+      ctxgs11.strokeStyle = 'white';
+      ctxgs11.lineWidth = 600;
+      ctxgs11.beginPath();
+      ctxgs11.moveTo(0, posY); /*  */
+      ctxgs11.lineTo(0, 0);
+      ctxgs11.stroke();
+    }
+
+    function moveLine() {
+      posY += speed;
+
+      if (posY < 0 || posY > canvass11.height) {
+        speed = speed * -1;
+      }
+    }
+
+    function loop() {
+      // clear old frame;
+      //ctxgs11.clearRect(0,0,canvass11.width, canvass11.height);
+      moveLine();
+      drawLine();
+      cancelani = requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
+  }
+ }
+
+}
+
+
+var imgobjplunger4=null;
+function injectplunger4(){
+ var plungertop = 10;
+ var imgplunger4= document.getElementById("plungerinj");
+ clearInterval(imgobjplunger4);
+ imgobjplunger4 = setInterval(frame, 80);
+
+
+ function frame(){
+  if(plungertop == 13){
+    clearInterval(imgobjplunger4);
+    document.getElementById("plungerinj").removeAttribute("onclick", "injectplunger4()");
+    document.getElementById("int40").style.display="block";
+  }
+  else{
+    plungertop++;
+    imgplunger4.style.top=plungertop + "%";
+
+    canvass11 = document.getElementById("injectioncolumn4");
+    ctxgs11 = canvass11.getContext("2d");
+    var posY = 0;
+    var speed = 0.5;
+
+
+
+    function drawLine() {
+
+
+      ctxgs11.strokeStyle = 'white';
+      ctxgs11.lineWidth = 600;
+      ctxgs11.beginPath();
+      ctxgs11.moveTo(0, posY); /*  */
+      ctxgs11.lineTo(0, 0);
+      ctxgs11.stroke();
+    }
+
+    function moveLine() {
+      posY += speed;
+
+      if (posY < 0 || posY > canvass11.height) {
+        speed = speed * -1;
+      }
+    }
+
+    function loop() {
+      // clear old frame;
+      //ctxgs11.clearRect(0,0,canvass11.width, canvass11.height);
+      moveLine();
+      drawLine();
+      cancelani = requestAnimationFrame(loop);
+    }
+    requestAnimationFrame(loop);
+  }
+ }
+
+}
 
 
 var thermogramData = [
